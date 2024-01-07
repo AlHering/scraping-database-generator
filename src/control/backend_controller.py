4b7407c2249774a7305baaa22844b0111868ebc1
Alexander Hering <dev.hering@gmail.com>
2024-01-07 12:28:11 +0100

    adjusting main controller

--- src/control/backend_controller.py
+++ src/control/backend_controller.py
@@ -13,16 +13,14 @@ from typing import Optional, Any, List, Dict, Union
 from src.configuration import configuration as cfg
 from src.utility.gold.basic_sqlalchemy_interface import BasicSQLAlchemyInterface, FilterMask as FilterMask
 from src.utility.bronze import sqlalchemy_utility
-from src.model.language_models.data_model import populate_data_instrastructure
-from src.model.language_models.llm_pool import ThreadedLLMPool
-from src.model.knowledgebase.knowledgebase_router import spawn_knowledgebase_instance
+from src.model.backend.data_model import populate_data_instrastructure
 from src.utility.silver import embedding_utility
 from src.utility.silver.file_system_utility import safely_create_path
 
 
-class FollowerLLMController(BasicSQLAlchemyInterface):
+class BackendController(BasicSQLAlchemyInterface):
     """
-    Controller class for Follower-LLM functionality.
+    Controller class for Scraping Database Generation functionality.
     """
 
     def __init__(self, working_directory: str = None, database_uri: str = None) -> None:
@@ -101,125 +99,6 @@ class FollowerLLMController(BasicSQLAlchemyInterface):
         Method for running shutdown process.
         """
         pass
-    """
-    LLM handling methods
-    """
-
-    def load_instance(self, lm_instance_id: Union[str, int]) -> Optional[str]:
-        """
-        Method for loading a configured language model instance.
-        :param lm_instance_id: Model instance ID.
-        :return: Model instance ID if process as successful.
-        """
-        lm_instance_id = str(lm_instance_id)
-        if lm_instance_id in self._cache["lms"]:
-            if not self.llm_pool.is_running(lm_instance_id):
-                self.llm_pool.start(lm_instance_id)
-                self._cache["lms"][lm_instance_id]["restarted"] += 1
-        else:
-            self._cache["lms"][lm_instance_id] = {
-                "started": None,
-                "restarted": 0,
-                "accessed": 0,
-                "inactive": 0
-            }
-            obj = self.get_object("lminstance", int(lm_instance_id))
-
-            self.llm_pool.prepare_llm({
-                "backend": obj.backend,
-                "model_path": obj.model_path,
-                "model_file": obj.model_file,
-                "model_parameters": obj.model_parameters,
-                "tokenizer_path": obj.tokenizer_path,
-                "tokenizer_parameters": obj.tokenizer_parameters,
-                "config_path": obj.config_path,
-                "config_parameters": obj.config_parameters,
-                "default_system_prompt": obj.default_system_prompt,
-                "use_history": obj.use_history,
-                "encoding_parameters": obj.encoding_parameters,
-                "generating_parameters": obj.generating_parameters,
-                "decoding_parameters": obj.decoding_parameters
-            }, lm_instance_id)
-            self.llm_pool.start(lm_instance_id)
-            self._cache["lms"][lm_instance_id]["started"] = dt.now()
-        return lm_instance_id
-
-    def unload_instance(self, lm_instance_id: Union[str, int]) -> Optional[str]:
-        """
-        Method for unloading a configured language model instance.
-        :param lm_instance_id: Config ID.
-        :return: Config ID if process as successful.
-        """
-        lm_instance_id = str(lm_instance_id)
-        if lm_instance_id in self._cache["lms"]:
-            if self.llm_pool.is_running(lm_instance_id):
-                self.llm_pool.stop(lm_instance_id)
-            return lm_instance_id
-        else:
-            return None
-
-    def forward_generate(self, lm_instance_id: Union[str, int], prompt: str) -> Optional[str]:
-        """
-        Method for forwarding a generate request to an instance.
-        :param lm_instance_id: Config ID.
-        :param prompt: Prompt.
-        :return: Config ID.
-        """
-        lm_instance_id = str(lm_instance_id)
-        self.load_instance(lm_instance_id)
-        return self.llm_pool.generate(lm_instance_id, prompt)
-
-    """
-    Knowledgebase handling methods
-    """
-
-    def register_knowledgebase(self,
-                               backend: str,
-                               knowledgebase_path: str,
-                               knowledgebase_parameters: dict = None,
-                               preprocessing_parameters: dict = None,
-                               embedding_parameters: dict = None,
-                               retrieval_parameters: dict = None,
-                               embedding_model_instance_id: int = None,
-                               owner_id: int = None,
-                               kb_instance_id: int = None) -> str:
-        """
-        Method for registering knowledgebase.
-        :param backend: Backend of the knowledgebase instance.
-        :param knowledgebase_path: Path of the knowledgebase instance.
-        :param knowledgebase_parameters: Parameters for the knowledgebase instantiation.
-            Defaults to None.
-        :param preprocessing_parameters: Parameters for document preprocessing.
-            Defaults to None.
-        :param embedding_parameters: Parameters for document embedding.
-            Defaults to None.
-        :param retrieval_parameters: Parameters for the document retrieval.
-            Defaults to None.
-        :param embedding_model_instance_id: ID of the model instance to use as default embedding model.
-            Defaults to None.
-        :param owner_id: ID of the creating user.
-            Defaults to None.
-        :param kb_instance_id: Knowledgebase instance ID.
-            Defaults to None in which case a new database entry is created.
-        """
-        kb_instance_id = self.post_object("kbinstance",
-                                          backend=backend,
-                                          knowledgebase_path=knowledgebase_path,
-                                          knowledgebase_parameters=knowledgebase_parameters,
-                                          preprocessing_parameters=preprocessing_parameters,
-                                          embedding_parameters=embedding_parameters,
-                                          retrieval_parameters=retrieval_parameters,
-                                          embedding_model_instance_id=embedding_model_instance_id,
-                                          owner_id=owner_id) if kb_instance_id is None else kb_instance_id
-        self._cache["kbs"][str(kb_instance_id)] = spawn_knowledgebase_instance(
-            backend=backend,
-            knowledgebase_path=knowledgebase_path,
-            knowledgebase_parameters=knowledgebase_parameters,
-            preprocessing_parameters=preprocessing_parameters,
-            embedding_parameters=embedding_parameters,
-            retrieval_parameters=retrieval_parameters,
-            embedding_model_instance_id=embedding_model_instance_id
-        )
 
     """ 
     Interaction methods
