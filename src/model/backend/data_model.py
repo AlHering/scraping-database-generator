de099a5fcfcce85fe21990793d563521bd76daaf
Alexander Hering <dev.hering@gmail.com>
2024-01-07 12:35:36 +0100

    built up data model

--- src/model/backend/data_model.py
+++ src/model/backend/data_model.py
@@ -33,24 +33,42 @@ def populate_data_instrastructure(engine: Engine, schema: str, model: dict) -> N
     schema = fix_schema(schema)
     base = declarative_base()
 
-    class Source(base):
+    class Log(base):
         """
-        Source class, representing an source.
+        Log class, representing an log entry.
         """
-        __tablename__ = f"{schema}source"
+        __tablename__ = f"{schema}log"
         __table_args__ = {
-            "comment": "Source table.", "extend_existing": True}
+            "comment": "Log table.", "extend_existing": True}
 
-        id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True,
-                    comment="ID of the source.")
-        url = Column(String, unique=True,
-                     comment="URL for the source.")
-        name = Column(String,
-                      comment="Display name for the source.")
-        scraping_metadata = Column(JSON,
-                                   comment="Metadata for scraping.")
-        info = Column(JSON,
-                      comment="Metadata of the source.")
+        id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False,
+                    comment="ID of the logging entry.")
+        request = Column(JSON, nullable=False,
+                         comment="Request, sent to the backend.")
+        response = Column(JSON, comment="Response, given by the backend.")
+        requested = Column(DateTime, server_default=func.now(),
+                           comment="Timestamp of request recieval.")
+        responded = Column(DateTime, server_default=func.now(), server_onupdate=func.now(),
+                           comment="Timestamp of reponse transmission.")
+
+    class GenerationProfile(base):
+        """
+        GenerationProfile class, representing an scraping database generation profile entry.
+        """
+        __tablename__ = f"{schema}generation_profile"
+        __table_args__ = {
+            "comment": "GenerationProfile table.", "extend_existing": True}
+
+        id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False,
+                    comment="ID of the entry.")
+        request_url = Column(String, nullable=False,
+                             comment="Request URL from which profile is generated.")
+        request_headers = Column(
+            JSON, comment="Request headers from which profile is generated.")
+
+        generation_profile = Column(
+            JSON, comment="Generation profile.")
+
         created = Column(DateTime, server_default=func.now(),
                          comment="Timestamp of creation.")
         updated = Column(DateTime, server_default=func.now(), server_onupdate=func.now(),
@@ -58,142 +76,7 @@ def populate_data_instrastructure(engine: Engine, schema: str, model: dict) -> N
         inactive = Column(Boolean, nullable=False, default=False,
                           comment="Inactivity flag.")
 
-        feeds = relationship(
-            "Feed", back_populates="source")
-        channels = relationship(
-            "Channel", back_populates="source")
-        assets = relationship(
-            "Asset", back_populates="source")
-
-    class Feed(base):
-        """
-        Feed class, representing an source feed.
-        """
-        __tablename__ = f"{schema}feed"
-        __table_args__ = {
-            "comment": "Feed table.", "extend_existing": True}
-
-        id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True,
-                    comment="ID of the feed.")
-        url = Column(String, unique=True,
-                     comment="URL for the feed.")
-        name = Column(String,
-                      comment="Display name for the feed.")
-        scraping_metadata = Column(JSON,
-                                   comment="Metadata for scraping.")
-        info = Column(JSON,
-                      comment="Metadata of the feed.")
-        created = Column(DateTime, server_default=func.now(),
-                         comment="Timestamp of creation.")
-        updated = Column(DateTime, server_default=func.now(), server_onupdate=func.now(),
-                         comment="Timestamp of last update.")
-        inactive = Column(Boolean, nullable=False, default=False,
-                          comment="Inactivity flag.")
-
-        source_id = mapped_column(
-            Integer, ForeignKey(f"{schema}source.id"))
-        source = relationship(
-            "Source", back_populates="feeds")
-
-    class Channel(base):
-        """
-        Channel class, representing an channel.
-        """
-        __tablename__ = f"{schema}channel"
-        __table_args__ = {
-            "comment": "Channel table.", "extend_existing": True}
-
-        id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True,
-                    comment="ID of the channel.")
-        url = Column(String, unique=True,
-                     comment="URL for the channel.")
-        name = Column(String,
-                      comment="Display name for the channel.")
-        scraping_metadata = Column(JSON,
-                                   comment="Metadata for scraping.")
-        info = Column(JSON,
-                      comment="Metadata of the channel.")
-        created = Column(DateTime, server_default=func.now(),
-                         comment="Timestamp of creation.")
-        updated = Column(DateTime, server_default=func.now(), server_onupdate=func.now(),
-                         comment="Timestamp of last update.")
-        inactive = Column(Boolean, nullable=False, default=False,
-                          comment="Inactivity flag.")
-
-        source_id = mapped_column(
-            Integer, ForeignKey(f"{schema}source.id"))
-        source = relationship(
-            "Source", back_populates="channels")
-        assets = relationship(
-            "Asset", back_populates="channel")
-
-    class Asset(base):
-        """
-        Asset class, representing an asset.
-        """
-        __tablename__ = f"{schema}asset"
-        __table_args__ = {
-            "comment": "Asset table.", "extend_existing": True}
-
-        id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True,
-                    comment="ID of the asset.")
-        url = Column(String, unique=True,
-                     comment="URL for the asset.")
-        scraping_metadata = Column(JSON,
-                                   comment="Metadata for scraping.")
-        info = Column(JSON,
-                      comment="Metadata of the asset.")
-        created = Column(DateTime, server_default=func.now(),
-                         comment="Timestamp of creation.")
-        updated = Column(DateTime, server_default=func.now(), server_onupdate=func.now(),
-                         comment="Timestamp of last update.")
-        inactive = Column(Boolean, nullable=False, default=False,
-                          comment="Inactivity flag.")
-
-        files = relationship(
-            "File", back_populates="asset")
-        source_id = mapped_column(
-            Integer, ForeignKey(f"{schema}source.id"))
-        source = relationship(
-            "Source", back_populates="assets")
-        channel_id = mapped_column(
-            Integer, ForeignKey(f"{schema}channel.id"))
-        channel = relationship(
-            "Channel", back_populates="assets")
-
-    class File(base):
-        """
-        File class, representing a file.
-        """
-        __tablename__ = f"{schema}document"
-        __table_args__ = {
-            "comment": "File table.", "extend_existing": True}
-
-        id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True,
-                    comment="ID of the file.")
-        path = Column(String, nullable=False,
-                      comment="Path of the file.")
-        encoding = Column(String, nullable=False,
-                          comment="Encoding of the file.")
-        extension = Column(String, nullable=False,
-                           comment="Extension of the file.")
-        url = Column(String,
-                     comment="URL for the file.")
-        sha256 = Column(Text,
-                        comment="SHA256 hash for the file.")
-        created = Column(DateTime, server_default=func.now(),
-                         comment="Timestamp of creation.")
-        updated = Column(DateTime, server_default=func.now(), server_onupdate=func.now(),
-                         comment="Timestamp of last update.")
-        inactive = Column(Boolean, nullable=False, default=False,
-                          comment="Inactivity flag.")
-
-        asset_id = mapped_column(
-            Integer, ForeignKey(f"{schema}asset.id"))
-        asset = relationship(
-            "Asset", back_populates="files")
-
-    for dataclass in [Source, Channel, Feed, Asset, File]:
+    for dataclass in [Log, GenerationProfile]:
         model[dataclass.__tablename__.replace(schema, "")] = dataclass
 
     base.metadata.create_all(bind=engine)
