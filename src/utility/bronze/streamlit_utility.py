877684d6de4688c719a1467a3ef54f9e96b2006c
Alexander Hering <dev.hering@gmail.com>
2024-01-08 15:54:13 +0100

    extracted utlity function

--- src/utility/bronze/streamlit_utility.py
+++ src/utility/bronze/streamlit_utility.py
@@ -7,6 +7,7 @@
 """
 from typing import Any
 from time import sleep
+import streamlit as st
 from datetime import datetime as dt
 
 
@@ -27,3 +28,30 @@ def wait_for_state_variable(streamlit_context: Any, variable_name: str, waiting_
             if timeout >= 0 and dt.now() - start >= timeout:
                 raise TimeoutError(
                     f"Timeout while waiting for '{variable_name}' to be available under Streamlit context.")
+
+
+def update_bound_state_dictionary(field: str, update: dict) -> None:
+    """
+    Function for updating a bound state managed dictionary.
+    :param field: State field of dictionary.
+    :param update: Streamlit update dictionary.
+    """
+    for entry in update["added_rows"]:
+        if len(entry.items()) > 1:
+            st.session_state[field].loc[entry["_index"],
+                                        ["value"]] = entry["value"]
+            update["added_rows"].remove(entry)
+
+    done = []
+    for entry_key in update["edited_rows"]:
+        if len(update["edited_rows"][entry_key].items()) > 1:
+            entry = update["edited_rows"][entry_key]
+            st.session_state[field].loc[entry["_index"],
+                                        ["value"]] = entry["value"]
+            done.append(entry_key)
+    for to_remove in done:
+        update["edited_rows"].pop(to_remove)
+    for entry_index in update["deleted_rows"]:
+        st.session_state[field].drop(
+            st.session_state[field].index[entry_index], inplace=True)
+    update["deleted_rows"] = []
