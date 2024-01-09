f93df511d5c474ac4cb9d1b9c84a2ff797cd0143
Alexander Hering <dev.hering@gmail.com>
2024-01-09 18:37:24 +0100

    extracted and restructured functionality

---
+++ src/view/streamlit_frontend/frontend_utility/frontend_rendering.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           Scraping Database Generator            *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import copy
+from typing import Any, List
+import streamlit as st
+from code_editor import code_editor
+from src.configuration import configuration as cfg
+from src.utility.bronze import json_utility, requests_utility
+from src.view.streamlit_frontend.frontend_utility.state_handling import update_state_cache
+
+
+###################
+# Helper functions
+###################
+
+
+def convert_response_name_to_label(response_name: str) -> str:
+    """
+    Function for converting response name to a label text.
+    :param response_name: Response name.
+    :return: Label text.
+    """
+    time_text, rest_text = response_name.split("_STATUS")
+    time_text = time_text.replace("_", " ").replace("-", ":")
+    status_text, *url_text = rest_text.split("_")
+    url_text = "_".join(url_text)
+    return f"{time_text} Status: {status_text}\n{url_text}"
+
+
+def get_json_editor_buttons() -> List[dict]:
+    """
+    Function for acquiring json payload code editor buttons.
+    :return: Buttons as list of dictionaries.
+    """
+    return [
+        {
+            "name": "save",
+            "feather": "Save",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": [
+                    "save-state",
+                    [
+                        "response",
+                        "saved"
+                    ]
+            ],
+            "response": "saved",
+            "style": {"top": "0rem", "right": "9.6rem"}
+        },
+        {
+            "name": "copy",
+            "feather": "Copy",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": ["copyAll"],
+            "style": {"top": "0rem", "right": "5rem"}
+        },
+        {
+            "name": "clear",
+            "feather": "X",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": ["selectall", "del", ["insertstring", "{\n\n\n\n}"], "save-state",
+                         ["response", "saved"]],
+            "style": {"top": "0rem", "right": "0.4rem"}
+        },
+    ]
+
+
+###################
+# Helper functions
+###################
+
+
+def render_sidebar_control_header() -> None:
+    """
+    Function for rendering the sidebar control header.
+    """
+    sidebar_right, sidebar_left = st.sidebar.columns([0.5, 0.5])
+    save_cache_button = sidebar_left.button("Save state")
+    if save_cache_button:
+        with st.spinner("Saving State..."):
+            json_utility.save(
+                st.session_state["CACHE"], cfg.PATHS.FRONTEND_CACHE)
+    clear_cache_button = sidebar_right.button("Clear state")
+    if clear_cache_button:
+        with st.spinner("Clearing State..."):
+            st.session_state["CACHE"] = copy.deepcopy(json_utility.load(
+                cfg.PATHS.FRONTEND_DEFAULT_CACHE))
+    st.sidebar.divider()
+
+
+def render_sidebar_response_list() -> None:
+    """
+    Function for rendering the sidebar response list.
+    """
+    for response_name in st.session_state["CACHE"]["responses"]:
+        if response_name != "default":
+            st.sidebar.button(convert_response_name_to_label(response_name),
+                              on_click=lambda x: update_state_cache({
+                                  "current_response": x
+                              }),
+                              args=(response_name, ))
+
+
+def render_request_input_form(parent_widget: Any) -> Any:
+    """
+    Function for rendering request input form.
+    :param parent_widget: Parent widget.
+    :return: Submit button widget.
+    """
+    request_form = parent_widget.form("request_update")
+
+    sending_line_left, sending_line_middle, sending_line_right = request_form.columns(
+        [0.18, 0.67, 0.15])
+
+    sending_line_left.selectbox("Method",
+                                key="method_update",
+                                options=list(
+                                    requests_utility.REQUEST_METHODS.keys()),
+                                index=list(
+                                    requests_utility.REQUEST_METHODS.keys()).index(st.session_state["CACHE"]["method"]))
+    sending_line_middle.text_input("URL",
+                                   key="url_update",
+                                   value=st.session_state.get(
+                                       "url_update", ""))
+    sending_line_right.markdown("## ")
+
+    submitted = sending_line_right.form_submit_button(
+        "Send")
+    request_form.divider()
+    request_form.markdown("##### Request Headers: ")
+    request_form.text(
+        """(Confirm with CTRL+ENTER or by pressing "save")""")
+    with request_form.empty():
+        code_editor("{\n\n\n\n}",
+                    key="headers_update",
+                    lang="json",
+                    allow_reset=True,
+                    options={"wrap": True},
+                    buttons=get_json_editor_buttons()
+                    )
+    request_form.divider()
+    request_form.markdown("##### Request Parameters: ")
+    request_form.text(
+        """(Confirm with CTRL+ENTER or by pressing "save")""")
+    with request_form.empty():
+        code_editor("{\n\n\n\n}",
+                    key="params_update",
+                    lang="json",
+                    allow_reset=True,
+                    options={"wrap": True},
+                    buttons=get_json_editor_buttons()
+                    )
+    request_form.divider()
+    request_form.markdown(
+        "##### Request JSON Payload:")
+    request_form.text(
+        """(Confirm with CTRL+ENTER or by pressing "save")""")
+    with request_form.empty():
+        code_editor("{\n\n\n\n}",
+                    key="json_payload_update",
+                    lang="json",
+                    allow_reset=True,
+                    options={"wrap": True},
+                    buttons=get_json_editor_buttons()
+                    )
+    return submitted
---
+++ src/view/streamlit_frontend/frontend_utility/state_handling.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           Scraping Database Generator            *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import os
+import json
+import streamlit as st
+from src.configuration import configuration as cfg
+from src.utility.bronze import json_utility
+
+
+def populate_state_cache() -> None:
+    """
+    Function for populating state cache.
+    """
+    if not os.path.exists(cfg.PATHS.RESPONSE_PATH):
+        os.makedirs(cfg.PATHS.RESPONSE_PATH)
+    with st.spinner("Loading State..."):
+        if os.path.exists(cfg.PATHS.FRONTEND_CACHE):
+            st.session_state["CACHE"] = json_utility.load(
+                cfg.PATHS.FRONTEND_CACHE)
+        else:
+            st.session_state["CACHE"] = json_utility.load(
+                cfg.PATHS.FRONTEND_DEFAULT_CACHE)
+
+
+def update_state_cache(update: dict) -> None:
+    """
+    Function for updating state cache.
+    :param update: State update.
+    """
+    for key in update:
+        st.session_state["CACHE"][key] = update[key]
+
+
+def trigger_state_dictionary_update() -> None:
+    """
+    Function for triggering an state dictionary update.
+    """
+    update_state_cache({
+        "method": st.session_state["method_update"],
+        "url": st.session_state["url_update"] if st.session_state["url_update"] else st.session_state["url"]
+    })
+
+    for state_dict in ["headers", "params", "json"]:
+        if st.session_state.get(f"{state_dict}_update") is None or not json_utility.is_json(st.session_state[f"{state_dict}_update"]["text"]):
+            update_state_cache({state_dict: {}})
+        else:
+            update_state_cache({state_dict: json.loads(
+                st.session_state[f"{state_dict}_update"]["text"])})
+
+
+def update_request_state() -> None:
+    """
+    Function for updating request state before sending off requests.
+    """
+    print("="*10 + "BEFORE" + "="*10)
+    print(st.session_state["CACHE"])
+    trigger_state_dictionary_update()
+    print("="*10 + "AFTER" + "="*10)
+    print(st.session_state["CACHE"])
