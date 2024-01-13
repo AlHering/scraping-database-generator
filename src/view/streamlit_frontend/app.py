f26dae1af4cf390f4039f15a10c14b6d21d5cfe9
Alexander Hering <dev.hering@gmail.com>
2024-01-13 18:09:29 +0100

    adjusted to functionality extraction

--- src/view/streamlit_frontend/app.py
+++ src/view/streamlit_frontend/app.py
@@ -11,10 +11,10 @@ import json
 import traceback
 import requests
 from urllib.parse import urlparse
-from http.client import responses as status_codes
 from typing import Any, Optional, List
 import streamlit as st
 from src.configuration import configuration as cfg
+from src.interfaces.frontend_interface import send_request
 from src.utility.bronze import json_utility, requests_utility, time_utility
 from src.view.streamlit_frontend.frontend_utility.state_cache_handling import populate_state_cache
 from src.view.streamlit_frontend.frontend_utility.frontend_rendering import render_request_input_form, render_response_data, render_sidebar_control_header, render_sidebar_response_list
@@ -41,55 +41,6 @@ def prepare_request_input() -> dict:
         except Exception:
             kwargs[field] = None
     return kwargs
-
-
-def send_request(method: str, url: str, headers: Optional[dict] = None, params: Optional[dict] = None, json_payload: Optional[dict] = None) -> None:
-    """
-    Function for sending off request.
-    :param method: Request method.
-    :param url: Target URL.
-    :param headers: Request headers.
-        Defaults to None.
-    :param params: Request parameters.
-        Defaults to None.
-    :param json_payload: JSON payload.
-        Defaults to None.
-    """
-    response_content = {}
-    response_status = -1
-    response_status_message = "An unknown error appeared"
-    response_headers = {}
-
-    response = None
-    try:
-        response = requests_utility.REQUEST_METHODS[method](
-            url=url,
-            params=params,
-            headers=headers,
-            json=json_payload
-        )
-        response_status = response.status_code
-        response_status_message = f"Status description: {status_codes[response_status]}"
-        response_headers = dict(response.headers)
-    except requests.exceptions.RequestException as ex:
-        response_status_message = f"Exception '{ex}' appeared.\n\nTrace:{traceback.format_exc()}"
-
-    if response is not None:
-        try:
-            response_content = response.json()
-        except json.decoder.JSONDecodeError:
-            response_content = response.text
-
-    handle_response_data(response_data={
-        "request_method": method,
-        "request_url": url,
-        "request_headers": headers,
-        "request_params": params,
-        "request_json_payload": json_payload,
-        "response": response_content,
-        "response_status": response_status,
-        "response_status_message": response_status_message,
-        "response_headers": response_headers})
 
 
 def handle_response_data(response_data: dict) -> None:
@@ -152,7 +103,8 @@ if __name__ == "__main__":
         st.session_state["first_sent"] = 1
         with data_fetching_spinner, st.spinner("Fetching data ..."):
             kwargs = prepare_request_input()
-            send_request(**kwargs)
+            response_data = send_request(**kwargs)
+            handle_response_data(response_data)
 
     render_response_data(right)
     render_sidebar_response_list()
