b4e93281bb9cd1c9122b80dec2632160fce6c8a4
Alexander Hering <dev.hering@gmail.com>
2024-01-14 23:06:02 +0100

    added runner script

---
+++ run_commandline_frontend.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           Scraping Database Generator            *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import os
+from src.view.commandline_frontend.app import run_session_loop
+
+
+if __name__ == "__main__":
+    run_session_loop()
