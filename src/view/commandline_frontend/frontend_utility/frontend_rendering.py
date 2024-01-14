731ea5a0094394ecd8d441dfa987db8b3eb397ff
Alexander Hering <dev.hering@gmail.com>
2024-01-14 23:10:02 +0100

    separated styles and made error box red

--- src/view/commandline_frontend/frontend_utility/frontend_rendering.py
+++ src/view/commandline_frontend/frontend_utility/frontend_rendering.py
@@ -7,7 +7,8 @@
 """
 from typing import List, Optional
 from rich.panel import Panel
-from prompt_toolkit.styles import Style
+from rich.style import Style as RichStyle
+from prompt_toolkit.styles import Style as PTStyle
 from src.view.commandline_frontend.frontend_utility.frontend_abstractions import Command
 
 
@@ -17,7 +18,7 @@ def get_error_page() -> dict:
     :return: Error page structure.
     """
     return {
-        "pre_panels": [Panel("""""", title="[red]Error")],
+        "pre_panels": [Panel("""""", title="[red]Error", border_style=RichStyle(color="red"))],
         "execute": [],
         "post_panels": [],
         "commands": [],
@@ -45,11 +46,11 @@ def get_bottom_toolbar() -> str:
     ]
 
 
-def get_style() -> Style:
+def get_style() -> PTStyle:
     """
     Function for getting style.
     :return: Style.
     """
-    return Style.from_dict({
+    return PTStyle.from_dict({
         "bottom-toolbar": "#333333 bg:#ffcc00"
     })
